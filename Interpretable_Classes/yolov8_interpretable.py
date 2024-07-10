d65d077355c2472bd931014210c1fde4f2057627
ajbax-cmd <ajbaxley42@gmail.com>
2024-07-10 02:52:27 -0400

    fixed bounding boxes

--- Interpretable_Classes/yolov8_interpretable.py
+++ Interpretable_Classes/yolov8_interpretable.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import torch
+from torch import nn
 from ultralytics import YOLO
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -8,14 +9,19 @@ from PIL import Image, ImageDraw
 import numpy as np
 from scipy.stats import pearsonr
 from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import CCA
 from interpretable_dataset import InterpretableDataset
 from faiss_indexer import FaissIndexer
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-class InterpretableYOLOTest(YOLO):
-    def __init__(self, data_yaml_path, weights, batch_size=1, img_size=(640, 640), target_layer_index=0):
-        super().__init__(weights)
-        self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model.to(self._device)
+class InterpretableYOLOTest(nn.Module):
+    def __init__(self, data_yaml_path, model, batch_size=1, img_size=(640, 640), target_layer_index=0):
+        super().__init__()
+        self.model = model
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model.to(self.device)
+        self.img_size=img_size[0]
         self.data_yaml_path = data_yaml_path
         self.data_yaml = self.load_yaml(data_yaml_path)
         self.train_loader, self.val_loader, self.test_loader = self.load_data(batch_size, img_size)
@@ -67,10 +73,10 @@ class InterpretableYOLOTest(YOLO):
         self.model.eval()
         with torch.no_grad():
             for sample_idx, (images, labels) in enumerate(self.train_loader):
-                images = images.to(self._device)
+                images = images.to(self.device)
                 cropped_images = self.crop_to_bounding_box(images, labels)
-                cropped_images = cropped_images.to(self._device)
-                outputs = self.model(cropped_images)  # Forward pass with cropped images
+                cropped_images = cropped_images.to(self.device)
+                outputs = self.model(images)  # Forward pass with cropped images
                 predictions = outputs[0] if isinstance(outputs, tuple) else outputs
                 self.predictions.extend(predictions.cpu().tolist())
                 batch_image_filenames = [os.path.basename(path) for path in self.train_loader.dataset.img_paths[sample_idx * self.train_loader.batch_size : (sample_idx + 1) * self.train_loader.batch_size]]
@@ -90,11 +96,10 @@ class InterpretableYOLOTest(YOLO):
 
             # Convert from (x_center, y_center, width, height) to (x1, y1, x2, y2)
             img_width, img_height = img.size(-1), img.size(-2)
-            if((x_center <= 1 and x_center >=0) and (y_center <=1 and y_center >=0)):
-                x_center *= img_width
-                y_center *= img_height
-                width *= img_width
-                height *= img_height
+            x_center *= img_width
+            y_center *= img_height
+            width *= img_width
+            height *= img_height
 
             x1 = int(x_center - width / 2)
             y1 = int(y_center - height / 2)
@@ -104,10 +109,14 @@ class InterpretableYOLOTest(YOLO):
             # Ensure coordinates are within the image bounds
             if x1 < 0: x1 = 0
             if y1 < 0: y1 = 0
-            if x2 >     x_center *= target_width
-    y_center *= target_height
-    width *= target_width
-    height *= target_heightg = Image.new("RGB", img_pil.size, (0, 0, 0))
+            if x2 > img_width: x2 = img_width
+            if y2 > img_height: y2 = img_height
+
+            img_pil = transforms.ToPILImage()(img).convert("RGB")
+            draw = ImageDraw.Draw(img_pil)
+
+            # Create a black image
+            black_img = Image.new("RGB", img_pil.size, (0, 0, 0))
 
             # Paste the black image on the original image outside the bounding box
             draw.rectangle([0, 0, img_pil.width, y1], fill=(0, 0, 0))
@@ -120,24 +129,24 @@ class InterpretableYOLOTest(YOLO):
 
         return torch.stack(cropped_images)
 
-
-
-
-
     def single_image_inference(self, image_path, k=5):
         self.training = False
         # Load and transform the image
         image = Image.open(image_path)
+
+        #if image.mode == 'L':
+            # Convert grayscale image to RGB by duplicating the single channel
+            #image = image.convert('RGB')
         transform = transforms.Compose([
             transforms.Resize((640, 640)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         image = transform(image)
-        image = image.unsqueeze(0)  # Add batch dimension
+        image = image.unsqueeze(0)  # Add batch dimension 
 
         # Move the image to the specified device
-        image = image.to(self._device)
+        image = image.to(self.device, non_blocking=True)
 
         # Ensure the model is in evaluation mode
         self.model.eval()
@@ -146,10 +155,9 @@ class InterpretableYOLOTest(YOLO):
         with torch.no_grad():
             outputs = self.model(image)  # Forward pass
 
-            # Extract relevant parts of the output
-            predictions = outputs[0].cpu().numpy()  # Assuming outputs[0] contains the predictions
-
-            if predictions.size == 0:
+            # Extract the output with highest confidence score
+            prediction = self.process_output(outputs)  # Assuming outputs[0] contains the predictions
+            if prediction.size == 0:
                 return {
                     'model_prediction': None,
                     'confidence_score': None,
@@ -158,23 +166,23 @@ class InterpretableYOLOTest(YOLO):
                     'distances': []
                 }
 
-            # Reshape predictions to (num_boxes, 5)
-            predictions = predictions[0].T  # Transpose to shape (8400, 5)
+  
 
             # YOLO output format: [x_center, y_center, width, height, confidence]
-            confidence_scores = predictions[:, 4]
-            bbox_predictions = predictions[:, :4]
+            best_confidence_score = prediction[4]
+            best_bbox = prediction[:4]
+            best_bbox = best_bbox / self.img_size  # Normalize
 
-            # Get the most confident prediction
-            best_idx = np.argmax(confidence_scores)
-            best_confidence_score = confidence_scores[best_idx]
-            best_bbox = bbox_predictions[best_idx]
-
-            # Normalize the bounding box coordinates
-            best_bbox = best_bbox / 640.0
 
             # Crop the image based on the bounding box coordinates
             cropped_image = self.crop_to_bounding_box(image, [best_bbox])
+            cropped_image = cropped_image.to(self.device)
+
+            #plt image here to check cropping worked
+
+            # Forward pass with the cropped image to get features from the target layer
+            #with torch.no_grad():
+            #    _ = self.model(cropped_image)  # Forward pass with cropped images
 
             # Get features from the target layer
             inference_features = self.inference_features  # Assuming the feature extraction is set up correctly
@@ -197,8 +205,6 @@ class InterpretableYOLOTest(YOLO):
 
         return result
 
-
-
     def iterate_directory_inference(self, directory_path, k=5):
         results = []
         for filename in os.listdir(directory_path):
@@ -208,6 +214,20 @@ class InterpretableYOLOTest(YOLO):
                 result['filename'] = filename  # Add the image path to the result for reference
                 results.append(result)
         return results
+    
+    def process_output(self, output):
+        # Assuming output tensor a shape of [1, 5, 8400]
+        output = output[0]
+        highest_confidence = 0
+        hc_index = 0
+        result = []
+        for i in range(0,8400,1):
+            if(output[0][4][i] > highest_confidence):
+                highest_confidence = output[0][4][i]
+                hc_index = i
+        for i in range(0, 5, 1):
+            result.append(output[0][i][hc_index].item())
+        return np.array(result)
 
     def insert_features_into_faiss(self):
         self.faiss_indexer.add_features(self.features)
@@ -279,14 +299,34 @@ class InterpretableYOLOTest(YOLO):
         print(f"Overall correlation score for the target layer: {overall_correlation_score}")
 
         return overall_correlation_score
+    
+    def calculate_cca_score(self):
+        # Ensure features and predictions are 2D arrays
+        assert self.features.ndim == 2, "Features should be a 2D array"
+        assert self.predictions.ndim == 3, "Predictions should be a 3D array (samples, attributes, bounding boxes)"
+
+        # Select a relevant dimension from predictions, e.g., confidence scores (5th attribute in this case)
+        selected_predictions = self.predictions[:, 4, :]  # Shape: (samples, bounding boxes)
+        selected_predictions = selected_predictions.mean(axis=1)  # Average over bounding boxes, resulting in shape: (samples,)
+
+        cca = CCA(n_components=1)
+        cca.fit(self.features, selected_predictions.reshape(-1, 1))
+        X_c, Y_c = cca.transform(self.features, selected_predictions.reshape(-1, 1))
+
+        correlation_score = np.corrcoef(X_c.T, Y_c.T)[0, 1]
+
+        print(f"CCA correlation score for the target layer: {correlation_score}")
+
+        return correlation_score
 
 
 
 def main():
     data = '/home/alan/Documents/YOLOV8_interpretable/Dataset_1/brain-tumor-detection-dataset/Br35H-Mask-RCNN/data.yaml'
     weights = '/home/alan/Documents/YOLOV8_interpretable/YOLOv8_weights/yolov8m_br35h.pt'
+    model = YOLO(weights)
 
-    model = InterpretableYOLOTest(data, weights, target_layer_index=76)
+    model = InterpretableYOLOTest(data, model, target_layer_index=76)
     
     
     # Perform inference on all images in a directory
@@ -295,5 +335,5 @@ def main():
     #for result in results:
         #print(result)
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
